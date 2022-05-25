b9c36986b7544f628dccff683857561a29b49718
diceuga <jycgt225@yahoo.co.jp>
2022-05-25 11:49:15 +0900

    2022052501

---
+++ README.md
@@ -0,0 +1,9 @@
+- ipfix-decorder
+  - Decode ipfix
+    - enterprise items are not supported
+    - support data type : unsigned and dateTimeMilliseconds
+    - output  : stdout, elasticsearch, influxdb(v6 only)
+  - Usage
+    - put ipfix-decorder.py and ipfixdef.py and config.yaml at same directory
+    - do "python3 ipfix-decorder.py
+
--- ipfix-decorder.py
+++ ipfix-decorder.py
@@ -8,6 +8,7 @@ import os
 import json
 import datetime
 import yaml
+import pprint
 from elasticsearch import Elasticsearch
 from ipfixdef import IPFIX_DEF
 
@@ -34,76 +35,98 @@ def getipv4(l,s):
   return(str(ipaddress.IPv4Address((nh[0]<<24)+(nh[1]<<16)+(nh[2]<<8)+nh[3])))
 
 def getdatefrommilli(t):
-  return (str(datetime.datetime.fromtimestamp(32400 + t/1000)))
+  return (str(datetime.datetime.fromtimestamp(timeadj + t/1000)))
 
   
 # env ------------------------------------------------------
 ## get from config.yaml
-config = {}
+conf = {}
 if ( os.path.isfile("config.yaml") ):
   with open("config.yaml", "r") as f:
-    config = yaml.load(f)
+    conf = yaml.load(f)
 
 # get from env
 envd = os.environ
 
-  # port
-  port = int(envd["flow_port"])      if ( "flow_port" in envd )    else 9005
+# prefer config.yaml
+# local dump
+dump = bool(conf["flow_dump"]) if ( "flow_dump" in conf ) else bool(envd["flow_dump"]) if ( "flow_dump" in envd ) else False
+pp = pprint.PrettyPrinter(indent=4)
 
-  # influxdb
-  idb =envd["flow_idb"]              if ( "flow_idb" in envd )     else False
-  idbaddr =envd["flow_idbaddr"]      if ( "flow_idbaddr" in envd ) else '127.0.0.1'
-  idbport =int(envd["flow_idbport"]) if ( "flow_idbport" in envd ) else 8086
-  if ( "flow_idbdbn" in envd ): idbdbn =envd["flow_idbdbn"]
-  else: idbdbn ='nf'
+# port
+port = int(conf["flow_port"]) if ( "flow_port" in conf ) else int(envd["flow_port"]) if ( "flow_port" in envd ) else 9005
+
+# influxdb
+idbflg = bool(conf["flow_idb"])  if ( "flow_id"   in conf ) else bool(envd["flow_idb"]) if ( "flow_idb" in envd ) else False
+if ( idbflg ):
+  idbaddr = conf["flow_idbaddr"]      if ( "flow_idbaddr" in conf ) else envd["flow_idbaddr"]      if ( "flow_idbaddr" in envd ) else ""
+  idbport = int(conf["flow_idbport"]) if ( "flow_idbport" in conf ) else int(envd["flow_idbport"]) if ( "flow_idbport" in envd ) else 0
+  idbdbn  = conf["flow_idbdbn" ]      if ( "flow_idbdbn"  in conf ) else envd["flow_idbdbn"]       if ( "flow_idbdbn" in envd )  else ""
+else:
+  idbaddr = ""
+  idbport = 0
+  idbdbn  = ""
+
+if ( ( idbaddr == "" ) or ( idbport == 0 ) or ( idbdbn == "") ):
+ idbflg = False
 
 # elastic search
-if ( "flow_ela" in envd ): ela =envd["flow_ela"]
-else: elaaddr = False
-if ( "flow_elaaddr" in envd ): elaaddr =envd["flow_elaaddr"]
-else: elaaddr = '127.0.0.1'
-if ( "flow_elaport" in envd ): elaport =int(envd["flow_elaport"])
-else: elaport = 8086
-if ( "flow_elaidx" in envd ): eladbn =envd["flow_elaidx"]
-else: elaidx ='nf'
+elaflg = bool(conf["flow_ela"])  if ( "flow_ela"  in conf ) else bool(envd["flow_ela"]) if ( "flow_ela" in envd ) else False 
+if ( elaflg ):
+  elaaddr = conf["flow_elaaddr"]      if ( "flow_elaaddr" in conf ) else envd["flow_elaaddr"]      if ( "flow_elaaddr" in envd ) else ""
+  elaport = int(conf["flow_elaport"]) if ( "flow_elaport" in conf ) else int(envd["flow_elaport"]) if ( "flow_elaport" in envd ) else 0
+  elauser = conf["flow_elauser"]      if ( "flow_elauser" in conf ) else envd["flow_elauser"]      if ( "flow_elauser" in envd ) else ""
+  elapass = conf["flow_elapass"]      if ( "flow_elapass" in conf ) else envd["flow_elapass"]      if ( "flow_elapass" in envd ) else ""
+  elaidx = conf["flow_elaidx"]        if ( "flow_elaidx" in conf )  else envd["flow_elaidx"]       if ( "flow_elaidx" in envd )  else ""
+else:
+  elaaddr = ""
+  elaport = 0
+  elauser = ""
+  elapass = ""
+  elaidx  = ""
 
-# elasticsearch
+if ( ( elaaddr == "" ) or ( elaport == 0 ) or ( elauser == "" ) or ( elapass == "" ) or ( elaidx == "" ) ):
+  elaflg = False
+
+# time_adjust(def JST(+9))
+timeadj  = bool(conf["flow_time"]) if ( "flow_time" in conf ) else bool(envd["flow_time"]) if ( "flow_time" in envd ) else 32400
+
+
 
 ##### bind/connection
 #host   = '10.2.255.102' #accept dest address,  is any
 host   = '0.0.0.0' #accept dest address,  is any
 locaddr = (host,port)   
-
-# udp socket
 sock = socket(AF_INET, SOCK_DGRAM); sock.bind(locaddr);
 
 # connect influxdb
-idb = influxdb.InfluxDBClient(host=idbaddr, port=idbport)
-#idb.create_database(idbdbn)
-idb.switch_database(idbdbn)
+if ( idbflg ):
+  idb = influxdb.InfluxDBClient(host=idbaddr, port=idbport)
+  idb.switch_database(idbdbn)
 
 # ela
-es = Elasticsearch("http://"+ elaaddr + ":9200",basic_auth=("elastic", "LLs_Q_WJFjkJGlwsHxv+"))
+if ( elaflg ):
+  es = Elasticsearch("http://"+ elaaddr + ":" + str(elaport) ,basic_auth=(elauser, elapass))
 
-dtp = {}
-otp = {}
-op  = {}
 
 # loop
-M_SIZE = 65535          #buffer
+dtp = {}		# data template
+otp = {}		# option template
+op  = {}		# option record
+M_SIZE = 65535          # buffer
+
 while True:
   try:
     message, cli_addr = sock.recvfrom(M_SIZE)
-    sa = cli_addr[0]		# source address
+    sa = cli_addr[0]	# source address
     if not ( sa in dtp ): dtp[sa] = {}
     if not ( sa in otp ): otp[sa] = {}
     if not ( sa in op ):  op[sa]  = {}
     
     # netflow header(for x)
     nh = struct.unpack('>HHLLLHH', message[0:20])
-    etime= nh[2]         # export time
+    etime= nh[2] 	# export time
     etime_f  = datetime.datetime.fromtimestamp(etime).isoformat()
-    #print(etime_f)
     seq  = nh[3]         # Sequence
     did  = nh[4]         # doamin id
     sid  = nh[5]         # set id
@@ -112,7 +135,8 @@ while True:
     doccommon = {
       "host"   : sa,
       "domainid" :did,
-      "setid":sid,
+      "setid": sid,
+      "sequence" : seq,
       "@timestamp" : etime_f,
       "exporttime": etime
     }
@@ -165,7 +189,10 @@ while True:
 
       dtp[sa][did][tid] = wktp  
 
-      res=es.index(index='flow_data4', document=doc)
+      if ( elaflg ):
+        res=es.index(index='flow_data4', document=doc)
+      if ( dump ):
+        pp.pprint (doc)
          
     # option template  
     elif (sid == 3 ):
@@ -215,7 +242,10 @@ while True:
 
       otp[sa][did][tid] = wktp  
 
-      res=es.index(index='flow_data4', document=doc)
+      if ( elaflg ):
+        res=es.index(index='flow_data4', document=doc)
+      if ( dump ):
+        pp.pprint (doc)
     
     else: # Flow Data
       pos = 20
@@ -265,10 +295,14 @@ while True:
                      }
                    }
                 ]
-                idb.write_points(json_body)
+                if ( idbflg ):
+                  idb.write_points(json_body)
 
               doc["packets"] = pc; doc["octets"] = bc;
-              res=es.index(index='flow_data4', document=doc)
+              if ( elaflg ):
+                res=es.index(index='flow_data4', document=doc)
+              if ( dump ):
+                pp.pprint (doc)
             
       elif (sid in otp[sa][did]):   # Option data
         doc = doccommon
@@ -291,8 +325,10 @@ while True:
               doc.update({ i["key"] : "unsupported type(" + i["type"] + ")" })
             pos = pos + i["len"]
 
-          #print(doc)
-          res=es.index(index='flow_data4', document=doc)
+          if ( elaflg ):
+            res=es.index(index='flow_data4', document=doc)
+          if ( dump ):
+            pp.pprint (doc)
 
       #else:
       #  print("template not found")
